30378fe8e267949f3c07a2a33a2aa9abefd5c2db
Kate <katekeiroz@gmail.com>
2021-05-09 23:23:59 +0100

    primeiro commit

---
+++ js/scrip.js
@@ -0,0 +1,19 @@
+if(window.SimpleSlide) {
+
+new SimpleSlide({
+    slide: "quote",
+    time : 5000
+})
+
+new SimpleSlide({
+    slide: "gallery",
+    time : 8000,
+    nav : true
+})
+
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+
+}
